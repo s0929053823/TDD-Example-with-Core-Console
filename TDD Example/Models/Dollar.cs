22bd8818dd08911273cc8acd87f625007ca39684
NickLiu <stalinmalcontent2009@gmail.com>
2019-05-10 11:05:10 +0800

    Add dollar and unitTest

---
+++ TDD Example/Models/Dollar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_Example.Models
+{
+    public class Dollar
+    {
+        private int _amount;
+
+        public Dollar(int amount)
+        {
+            this._amount = amount;
+        }
+
+        public Dollar times(int multiplier)
+        {
+            return new Dollar(_amount * multiplier);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return _amount == dollar._amount;
+        }
+    }
+}
---
+++ TDD_Test/MoneyTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Example.Models;
+namespace TDD_Test
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Dollar product = five.times(2);
+            Assert.AreEqual(new Dollar(10), product);
+            product = five.times(3);
+            Assert.AreEqual(new Dollar(15), product);
+        }
+
+        [TestMethod]
+        public void TestEquality()
+        {
+            Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
+            Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+        }
+    }
+}
